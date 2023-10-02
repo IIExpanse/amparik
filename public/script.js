f1fd3d7a925af8e8d9891f6ba8d602896cfb1a91
Alexander Bulatov <aleks40k@yandex.ru>
2023-10-02 19:08:00 +0300

    feat: оформлена карточка маршрута к магазину

--- public/script.js
+++ public/script.js
@@ -1,12 +1,18 @@
 var storeImage = document.getElementById("store-image");
 var pathImage = document.getElementById("path-image")
+var pathText = document.getElementById("path-text")
 var maxStoreImages = 5;
 var maxPathImages = 5;
 var storeCount = 2;
 var pathCount = 2;
 
-var pathGuidance = [
-
+var pathDescription = [
+    "Выйдите на станции метро «Медведково».\nПосле выхода из поезда, поверните направо.",
+    "Поднимитесь по эскалатору.\nПосле выхода из метро, поверните налево",
+    `Идите прямо до конца улицы, пока не выйдете на Т-образный перекресток.
+     Жилой дом, в котором находится наш магазин, можно увидеть ещё при выходе из метро!`,
+     "На перекрестке сверните налево и пройдите к торцу дома на углу.",
+     `Наш магазин находится чуть дальше вдоль дома, вы узнаете его по вывеске.\nЖдем Вас!`
 ]
 
 function switchStoreImage(val) {
@@ -31,4 +37,5 @@ function switchPathImage(val) {
         pathCount = 1;
     }
     pathImage.src = "images/path/path" + pathCount + ".jpg";
+    pathText.innerText = pathDescription[pathCount - 1];
 }
